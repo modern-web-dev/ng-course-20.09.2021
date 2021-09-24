30245e2e5135e7d4336cd703fa20de6b79a3e207
Marek Matczak <marek.matczak79@gmail.com>
2021-09-24 14:57:18 +0200

    forms part 2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,22 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
+import {ErrorMessagesComponent} from './forms/error-messages/error-messages.component';
+
+const exportedModules = [CommonModule, RouterModule, ReactiveFormsModule];
+const exportedComponents = [ErrorMessagesComponent];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrorMessagesComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [CommonModule, RouterModule, ReactiveFormsModule]
+  exports: [
+    ...exportedModules,
+    ...exportedComponents
+  ]
 })
 export class SharedModule {
 }
