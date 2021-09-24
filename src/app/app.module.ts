f13cb9103b6d585a60890cdf533460f4a5be17e5
Marek Matczak <marek.matczak79@gmail.com>
2021-09-24 15:38:13 +0200

    communication backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {BookModule} from './book/book.module';
-import {Router, RouterModule} from '@angular/router';
+import {RouterModule} from '@angular/router';
 import {CoreModule} from './core/core.module';
 import {bookRoutes} from './book/book.routes';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {bookRoutes} from './book/book.routes';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     CoreModule,
     RouterModule.forRoot([
       {path: '', redirectTo: '/books', pathMatch: 'full'},
--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -1,64 +1,27 @@
 import {Book, BookProps} from '../model/book';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {delay} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
+@Injectable()
 export class BookService {
-  private idSeq = 0;
-
-  private booksSubject = new BehaviorSubject<Book[]>([
-    {
-      id: this.idSeq++,
-      author: 'Douglas Crockford',
-      title: 'JavaScript. The Good Parts'
-    },
-    {
-      id: this.idSeq++,
-      author: 'Marek Matczak',
-      title: 'Angular for nerds'
-    },
-    {
-      id: this.idSeq++,
-      author: 'Tom Hombergs',
-      title: 'Get You Hands Dirty on Hexagonal Architecture'
-    }
-  ]);
+  constructor(private http: HttpClient) {
+  }
 
   update(bookToUpdate: Book): Observable<Book> {
-    return new Observable<Book>(subscriber => {
-      const bookCopy: Book = {...bookToUpdate};
-      const currentBooks = this.booksSubject.getValue();
-      const newBooks = currentBooks.map(book => bookToUpdate.id === book.id ? bookCopy : book);
-      this.booksSubject.next(newBooks);
-      subscriber.next(bookCopy);
-      subscriber.complete();
-    });
+    return this.http.put<Book>(`api/books/${bookToUpdate.id}`, bookToUpdate);
   }
 
   save(bookToSave: BookProps): Observable<Book> {
-    return new Observable<Book>(subscriber => {
-      const newBook = {id: this.idSeq++, ...bookToSave};
-      const currentBooks = this.booksSubject.getValue();
-      const newBooks = [...currentBooks, newBook];
-      this.booksSubject.next(newBooks);
-      subscriber.next(newBook);
-      subscriber.complete();
-    })
+    return this.http.post<Book>('api/books', bookToSave);
   }
 
   findAll(): Observable<Book[]> {
-    return this.booksSubject.asObservable();
+    return this.http.get<Book[]>('api/books');
   }
 
   getOne(bookId: number): Observable<Book> {
-    return new Observable<Book>(subscriber => {
-      const currentBooks = this.booksSubject.getValue();
-      const foundBook = currentBooks.find(book => book.id === bookId);
-      if (foundBook) {
-        subscriber.next(foundBook);
-        subscriber.complete();
-      } else {
-        subscriber.error(`Book with ID: ${bookId} could not be found!`);
-      }
-    });
+    return this.http.get<Book>(`api/books/${bookId}`);
   }
 }
